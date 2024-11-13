ba389dfb413afd31db71a06cc694343f7bffcbb2
ilhanigit <ibrahimilhan1999@gmail.com>
2024-11-13 20:34:10 +0300

    phonebook project deployed on aws

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "ilhanigit"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "instacekey"
+}
+
+variable "hosted-zone" {
+  default = "ibrahimilhan.com"
+}
